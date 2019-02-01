d0806cec5675694268e9e736a2ca67c924a17c11
Sydorov Leonid <info@space-ship.xyz>
2019-02-01 12:17:24 +0200

    Update contact info & optimization

--- index.php
+++ index.php
@@ -1,29 +1,25 @@
 <!doctype html>
 <html>
 <head>
-	<meta charset="windows-1251">
-	<title>Sidorov Leonid | Space-Ship.xyz</title>
+	<meta charset="utf-8">
+	<title>Sydorov Leonid | Space-Ship.xyz</title>
     
     <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
 	<meta name="viewport" content="width=1024px, user-scalable=no">
 
 	<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="css/styles.css" />
-	
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-	<script type="text/javascript" src="js/scripts.js"></script>
 
 </head>
 
 <body>
 
-	<!--<div id="rotate">
-		Пожалуйста, поверните своё устройство!
-	</div>-->
-
-	<div id="info">
-		<h1>Sidorov Leonid</h1>
-		<a href="mailto:info@space-ship.xyz"><h3>info@space-ship.xyz</h3></a>
+	<div id="info" class="glitch">
+		<h1>Sydorov Leonid</h1>
+		<a href="mailto:info@space-ship.xyz"><h2>info@space-ship.xyz</h2></a>
+		<h3>+38 (099) 04-09-624</h3>
+		<h4>+7 (911) 001-61-45</h4>
+		<a href="https://t.me/Serpanok" target="_blank"><h3>t.me/Serpanok</h3></a>
 	</div>
 
 	<div id="loading"></div>
@@ -40,20 +36,20 @@
 			<div class="bg"></div>
 		</div>
 		<div class="layer cloud" data-depth="0.90">
-			<div style="position: absolute; left: 30%; top: 20%; opacity: 0.8;">
+			<div style="position: absolute; left: 30%; top: 20%; opacity: 0.9;">
 				<span>PHP</span>
 				<img src="images/cloud1.png" />
 			</div>
 		</div>
 		<div class="layer cloud" data-depth="0.70">
-			<div style="position: absolute; left: 50%; top: 45%; opacity: 0.9;">
-				<span>Java</span>
+			<div style="position: absolute; left: 50%; top: 45%; opacity: 0.7;">
+				<span>SQL</span>
 				<img src="images/cloud2.png" />
 			</div>
 		</div>
 		<div class="layer cloud" data-depth="0.80">
-			<div style="position: absolute; left: 60%; top: 30%; opacity: 0.6;">
-				<span>SQL</span>
+			<div style="position: absolute; left: 60%; top: 30%; opacity: 0.8;">
+				<span>React<i style="font-size: 60%; font-style: normal;">Native</i></span>
 				<img src="images/cloud3.png" />
 			</div>
 		</div>
@@ -64,19 +60,19 @@
 			</div>
 		</div>
 		<div class="layer cloud" data-depth="0.30">
-			<div style="position: absolute; left: 10%; top: 70%; opacity: 0.7;">
+			<div style="position: absolute; left: 10%; top: 70%; opacity: 0.9;">
 				<span>JavaScript</span>
 				<img src="images/cloud1.png" />
 			</div>
 		</div>
 		<div class="layer cloud" data-depth="0.80">
-			<div style="position: absolute; left: 40%; top: 80%; opacity: 0.9;">
+			<div style="position: absolute; left: 40%; top: 80%; opacity: 0.8;">
 				<span>CSS</span>
 				<img src="images/cloud2.png" />
 			</div>
 		</div>
 		<div class="layer cloud" data-depth="0.50">
-			<div style="position: absolute; left: 20%; top: 50%; opacity: 0.8;">
+			<div style="position: absolute; left: 20%; top: 50%; opacity: 0.7;">
 				<span>HTML</span>
 				<img src="images/cloud3.png" />
 			</div>
@@ -87,14 +83,39 @@
 		Space-Ship.xyz &copy; All rights reserved
 	</div>
 
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script type="text/javascript" src="js/scripts.js"></script>
 	<script src="js/parallax.js"></script>
-	<script>
+	<script type="text/javascript">
+		const scene = document.getElementById('parallax');
+		const parallax = new Parallax(scene);
 
-	// Pretty simple huh?
-	var scene = document.getElementById('parallax');
-	var parallax = new Parallax(scene);
+		// Yandex.Metrika counter
+		(function (d, w, c) {
+			(w[c] = w[c] || []).push(function() {
+				try {
+					w.yaCounter36772575 = new Ya.Metrika({
+						id:36772575,
+						clickmap:true,
+						trackLinks:true,
+						accurateTrackBounce:true
+					});
+				} catch(e) { }
+			});
 
+			var n = d.getElementsByTagName("script")[0],
+				s = d.createElement("script"),
+				f = function () { n.parentNode.insertBefore(s, n); };
+			s.type = "text/javascript";
+			s.async = true;
+			s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+			if (w.opera == "[object Opera]") {
+				d.addEventListener("DOMContentLoaded", f, false);
+			} else { f(); }
+		})(document, window, "yandex_metrika_callbacks");
 	</script>
+	<noscript><div><img src="https://mc.yandex.ru/watch/36772575" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 
 </body>
 </html>
--- js/scripts.js
+++ js/scripts.js
@@ -2,20 +2,53 @@
 $(document).ready(function(){
 	
 	$(".cloud div span").hide(0);
-	
 	$("#loading").fadeOut(500);
 	
 	setTimeout(function(){
 		$(".cloud div span").fadeIn(500);
 	}, 3000);
 	
-	/*$(".cloud div").hover(function(){
-		alert(1);
-		$(this).children("span").fadeIn(100);
-	},
-	function(){
-		alert(0);
-		$(this).children("span").fadeOut(100);
-	});*/
+	$(".glitch").glitch({
+		layers: ["#03679B", "#58BDF1"],
+		offset: [40, 20],
+	});
 	
 });
+
+$.fn.glitch = function(options) {
+
+	let settings = $.extend({
+		layers: ["red", "green"],
+		offset: [10, 0],
+	}, options);
+
+	let content = $(this).html();
+	$(this).html("");
+	
+	for (var i = 0; i < settings.layers.length; ++i) {
+		$(this).append("<div class='layer' style='color:"+settings.layers[i]+";'>" + content + "</div>");
+	}
+	$(this).append("<div class='display'>" + content + "</div>");
+
+	let self = this;
+	let startTime = new Date().getTime();
+
+	this.render = function() {
+		let time = new Date().getTime() - startTime;
+		let visibility = Math.sin(time / 1) > 0.8;
+		self.find(".layer").each(function() {
+			let offset = [
+				visibility * ((Math.random() * 2 - 1) * settings.offset[0]),
+				visibility * ((Math.random() * 2 - 1) * settings.offset[1]),
+			];
+			$(this)
+				.css("top", offset[1] + "px")
+				.css("left", offset[0] + "px")
+				.css("opacity", 0.2 + visibility * 0.5);
+		});
+		window.setTimeout(self.render, 200);
+	};
+
+	window.setTimeout(this.render, 0);
+
+};
